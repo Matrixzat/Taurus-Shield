82e0714953e71e663d4238703aea88d6f7446560
Matrix Scraper <matrixzat99@gmail.com>
2026-03-17 22:36:26 +0000

    Use --unresolved-symbols=ignore-all so ICU symbols resolve at runtime from Android system libicuuc.so

--- scripts/build_blutter.sh
+++ scripts/build_blutter.sh
@@ -294,7 +294,7 @@ exec /usr/bin/cmake \\
     -DCMAKE_FIND_ROOT_PATH="${BLUTTER_DIR}/packages;${ANDROID_LIBS}" \\
     -DCMAKE_FIND_ROOT_PATH_MODE_PACKAGE=BOTH \\
     -DCMAKE_MODULE_PATH="/tmp/android_cmake_modules" \\
-    -DCMAKE_EXE_LINKER_FLAGS="-rdynamic -Wl,--allow-shlib-undefined -L${TOOLCHAIN}/sysroot/usr/lib/aarch64-linux-android -L${TOOLCHAIN}/sysroot/usr/lib/aarch64-linux-android/31" \\
+    -DCMAKE_EXE_LINKER_FLAGS="-rdynamic -Wl,--unresolved-symbols=ignore-all -L${TOOLCHAIN}/sysroot/usr/lib/aarch64-linux-android -L${TOOLCHAIN}/sysroot/usr/lib/aarch64-linux-android/31" \\
     -DPKG_CONFIG_USE_CMAKE_PREFIX_PATH=TRUE \\
     "\$@"
 CMAKEWRAP
